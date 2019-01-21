14c2de9d4ba633a0a3ef1a560c809a086e1856ea
Bryan Munene <bryanmunene@Bryans-MacBook-Pro.local>
2019-01-21 14:26:11 +0300

    Make first migration

--- DjangoArticlesTest/API/models.py
+++ DjangoArticlesTest/API/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+class Articles(models.Model):
+    """This class represent the Articles model."""
+
+    name = models.CharField(max_length=255, blank=False, unique=True)
+    text = models.TextField(blank=False)
+    author = models.CharField(max_length=255, blank=False, unique=True)
+    date_created = models.DateTimeField(auto_now_add=True)
+    date_modified = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        """Returns human readable representation of the model instance"""
+        return "{} authored by {}".format(self.text, self.name)
--- DjangoArticlesTest/API/tests.py
+++ DjangoArticlesTest/API/tests.py
@@ -1,3 +1,21 @@
 from django.test import TestCase
+from .models import Articles
 
-# Create your tests here.
+class ModelTestCase(TestCase):
+    """This is the base class of the tests for the Articles Model class. It defines the test suite"""
+
+    def setUp(self):
+        """Define the test client and other test variables."""
+        self.article_name = "Write world class code"
+        self.article_text = "To write world class code one needs to practice continuosly to master the craft"
+        self.article_author = "Test User"
+        self.article = Articles(name=self.article_name, text=self.article_text, author=self.article_author)
+
+    def test_model_can_create_an_article(self):
+        """Test the articles model can create an article."""
+        old_count = Articles.objects.count()
+        self.article.save()
+        new_count = Articles.objects.count()
+        self.assertNotEqual(old_count, new_count)
+
+    
